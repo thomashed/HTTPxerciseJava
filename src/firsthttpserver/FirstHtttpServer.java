1d27b9aea2735bd660f26be2e9fccec149b839c4
Thomas Hedegaard Jensen <ThomasHedegaard@Thomas-Hedegaard-Jensens-MacBook-Pro.local>
2014-08-29 10:43:49 +0200

    Updated http page
    - Added page that shows Request Headers

--- src/firsthttpserver/FirstHtttpServer.java
+++ src/firsthttpserver/FirstHtttpServer.java
@@ -7,6 +7,7 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.util.*;
 
 /**
  * @author Lars Mortensen
@@ -23,6 +24,7 @@ public class FirstHtttpServer {
         }
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
         server.createContext("/welcome", new RequestHandler());
+        server.createContext("/Headers", new RequestHandler());
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
@@ -34,6 +36,10 @@ public class FirstHtttpServer {
         public void handle(HttpExchange he) throws IOException {
 
             StringBuilder sb = new StringBuilder();
+
+            // The map
+            Map content = he.getRequestHeaders();
+
             sb.append("<!DOCTYPE html>\n");
             sb.append("<html>\n");
             sb.append("<head>\n");
@@ -41,9 +47,30 @@ public class FirstHtttpServer {
             sb.append("<meta charset='UTF-8'>\n");
             sb.append("</head>\n");
             sb.append("<body>\n");
+
+            sb.append("<table border = 1px>");
+            
+            sb.append("<tr>");
+            sb.append("<td> <h1>Headers</h1> </td>");
+            sb.append("<td> <h1>Values</h1> </td>");
+            sb.append("</tr>");
+            
+
+            for (Object key : content.keySet()) {
+                String keyValue = key.toString();
+                String value = content.get(key).toString();
+                sb.append("<tr>");
+                sb.append("<td>" + keyValue + "</td>");
+                sb.append("<td>" + value + "</td>");
+                sb.append("</tr>");
+            }
+
+            sb.append("</table>");
+
             sb.append("<h2>Welcome to my very first home made Web Server :-)</h2>\n");
             sb.append("</body>\n");
             sb.append("</html>\n");
+
             String response = sb.toString();
 
             Headers h = he.getResponseHeaders();
@@ -56,3 +83,15 @@ public class FirstHtttpServer {
         }
     }
 }
+
+// Something saved
+//            sb.append("<!DOCTYPE html>\n");
+//            sb.append("<html>\n");
+//            sb.append("<head>\n");
+//            sb.append("<title>My fancy Web Site</title>\n");
+//            sb.append("<meta charset='UTF-8'>\n");
+//            sb.append("</head>\n");
+//            sb.append("<body>\n");
+//            sb.append("<h2>Welcome to my very first home made Web Server :-)</h2>\n");
+//            sb.append("</body>\n");
+//            sb.append("</html>\n");
